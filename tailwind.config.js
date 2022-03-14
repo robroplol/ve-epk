fd4dbf8900dbf84d1254fd4a8d2760f2c3882c61
robroplol <robrop92@gmail.com>
2022-03-14 07:11:27 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "nunito": ["Nunito", "sans-serif"],
+        "permanentmarker": ["'Permanent Marker'"]
+      },
+    },
+  },
+  plugins: [],
+};
