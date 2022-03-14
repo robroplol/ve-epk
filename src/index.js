cf5de98421561d35a59e9dbb8915aaa3c78ae019
robroplol <robrop92@gmail.com>
2022-03-14 10:26:17 -0400

    add slider and avatar image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("test");
+const $ = require("jquery");
+const slick = require("slick-carousel");
+
+$(document).ready(function () {
+  $(".slide-container").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+  });
+});
